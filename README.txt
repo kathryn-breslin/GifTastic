565523f7fe9dfd77bf2f41d33ce47bc701f4dc9e
Kathryn Breslin <kathryn.l.breslin@gmail.com>
2018-07-13 16:15:07 -0500

    CSS Updates and working on image replacement

---
+++ README.txt
@@ -0,0 +1,11 @@
+This is a Gif finder!
+
+Double click any button at the top and watch 10 gifs populate the page.
+
+If you're interested in different gifs not shown at the top, enter it into the search bar and click the newly created button.
+
+You will see the title and the rating under each gif. 
+
+------------------------------------------------------
+
+Still working on replacing the still image with the animated gif and simplifying the click function so the user will not have to double click each button each time the search.
--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -5,12 +5,12 @@ var topics = ["plants", "books", "San Francisco",
 function displayButtons () {
    
         var topic = $(this).attr('data-name');
-        
+    
         //my GIPHY key would not work..the one used below is from our activities
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         topic + "&api_key=dc6zaTOxFJmzC&limit=10&rating=PG"
 
-        //MY GIPHY blow
+        //MY GIPHY below
         // var queryURL = "https://api.giphy.com/v1/gifs/search?" + topic + "api_key=HfzaLobXPlhuhwVrZxOCHvuJTyKDpo5m&q=topic&limit=10&offset=0&rating=PG&lang=en";
         // console.log("https://api.giphy.com/v1/gifs/search?" + topic + "api_key=HfzaLobXPlhuhwVrZxOCHvuJTyKDpo5m&q=topic&limit=10&offset=0&rating=PG&lang=en");
        
@@ -29,17 +29,27 @@ function displayButtons () {
                 topic = response.data;
                 for (var i = 0; i < topic.length; i++) {
                     var gifDiv = $('<div>');
-                    var p = $('<p>').text('Rating: ' + topic[i].rating);
+                    var rating = $('<p>').text('Rating: ' + topic[i].rating);
+                    var title = $('<p>').text('Title: ' + topic[i].title);
                     var gifImage = $('<img>');
-                    gifImage.attr('src', topic[i].images.fixed_height.url);
+                    gifImage.attr('src', topic[i].images.fixed_height_still.url);
                     gifDiv.addClass('gifImage');
                     gifDiv.append(gifImage);
-                    gifDiv.append(p);
+                    gifDiv.append(rating);
+                    gifDiv.append(title);
             
                     $('#gif-view').prepend(gifDiv);
+
                 }
-            });
 //each button needs to be double clicked in order for the collection of gifs to appear
+            $('.gifImage').on('click', function() {
+                var gifReplaceImage = gifImage.attr('src', topic[i].images.fixed_height.url);
+                gifImage.append(gifReplaceImage);
+            })
+                
+            });
+
+//change image to animated gif on click -- not working
     });
 }
 
